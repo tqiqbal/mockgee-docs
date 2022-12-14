c7b554a909ad1ac8c24245140ff5b04f98055577
tanacute <tan.acute@gmail.com>
2022-12-14 10:49:49 +0300

    Improved the documentation

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,15 @@
 export default defineAppConfig({
   docus: {
     title: 'Mockgee',
-    description: 'The best place to start your documentation.',
+    description: 'Lets mock API.',
     // image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'mockgee',
-      linkedin: 'mockgee',
+      linkedin: {
+        label: 'linkedin',
+        icon: 'mdi:linkedin',
+        href: 'https://www.linkedin.com/company/mockgee'
+      }
     },
     aside: {
       level: 0,
